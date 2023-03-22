a4328a8f88023c0099949e120e2119fcf4471979
Veronica Stork <zealous.anemone@gmail.com>
2023-03-21 22:11:19 -0400

    Adds basic components

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="header">Header</div>
+      <div className="content">Content</div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+const List = (props) => {
+
+  return (
+    <div className="list">
+      <Card />
+    </div>
+  )
+}
+
+export default List;
