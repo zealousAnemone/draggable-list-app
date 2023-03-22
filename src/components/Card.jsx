d982536c0ca8aff57757815462fe9923e6decc8a
Veronica Stork <zealous.anemone@gmail.com>
2023-03-21 23:20:47 -0400

    Adds ability to edit card, adds handle to draggable cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
 import Draggable from 'react-draggable';
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 const Card = (props) => {
   const nodeRef = useRef(null);
+  const [text, setText] = useState('Write something here.');
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
-    <Draggable nodeRef={nodeRef}>
+    <Draggable nodeRef={nodeRef} handle=".header">
       <div className="card" ref={nodeRef}>
         <div className="header">{props.title}</div>
-        <div className="content">Content</div>
+        <div className="content">{isEditing ? <><textarea className="text-input" value={text} onChange={(e) => setText(e.target.value)} onDoubleClick={() => setText('')} /><button onClick={() => setIsEditing(false)}>Save</button></> : <p onClick={() => setIsEditing(true)}>{text}</p>}
+</div>
       </div>
     </Draggable>
   
